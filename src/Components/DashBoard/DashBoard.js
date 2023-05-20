2de433e38b67b1bad50ab020c5ae03988b29e376
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2023-05-20 14:44:25 +0600

    user role update

--- src/Components/DashBoard/DashBoard.js
+++ src/Components/DashBoard/DashBoard.js
@@ -1,16 +1,20 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
+
 import { AuthContext } from "../UseContex/AuthProvider";
 import UserRole from "./UserRole";
 import logo from "../../assets/logo2.png";
-import DashBoadCommonInfo from "./DashBoadCommonInfo";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import {
+  FaCarAlt,
+  FaHeart,
+  FaRegPlusSquare,
+  FaUserAlt,
+  FaWeightHanging,
+} from "react-icons/fa";
 
 const DashBoard = () => {
   const { user, logout } = useContext(AuthContext);
-  const [userRole, setUserRole] = useState([]);
+  const [userRole, setUserRole] = useState({});
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -24,6 +28,8 @@ const DashBoard = () => {
       });
   }, [user?.email]);
 
+  console.log(userRole);
+
   const HandleLogOut = () => {
     logout()
       .then(() => {
@@ -36,14 +42,6 @@ const DashBoard = () => {
   return (
     <div>
       <div className="md:w-9/12 mx-auto ">
-        {/* <div>
-          <Link to="/">
-            <h2 className="mb-5 text-2xl font-bold">
-              <img src={logo} alt="" className="w-16 h-16" />
-            </h2>
-          </Link>
-        </div> */}
-
         <div className="flex flex-col items-center  md:flex-row  gap-10">
           <div className=" basis-3/12 text-xl font-bold h-screen shadow-lg pt-10 pl-5  ">
             <Link to="/">
@@ -60,9 +58,88 @@ const DashBoard = () => {
               <h2 className="text-xl font-bold">{user?.displayName}</h2>
             </div>
 
-            {userRole?.map((usersRole) => (
-              <UserRole key={usersRole._id} usersRole={usersRole}></UserRole>
-            ))}
+            <div>
+              {userRole?.role === "Seller" ? (
+                <>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "text-blue-400 font-bold" : "text-black-500 "
+                    }
+                    to="addservice"
+                  >
+                    <p className="mb-2">
+                      {" "}
+                      <FaRegPlusSquare className="inline-block mb-2 mr-2"></FaRegPlusSquare>{" "}
+                      Add Products
+                    </p>
+                  </NavLink>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "text-blue-400 font-bold" : "text-black-500 "
+                    }
+                    to="myservice"
+                  >
+                    <p className="mb-2">
+                      {" "}
+                      <FaWeightHanging className="inline-block mb-2 mr-2"></FaWeightHanging>{" "}
+                      My Products
+                    </p>
+                  </NavLink>
+                </>
+              ) : (
+                <></>
+              )}
+              {userRole?.role === "Buyer" && (
+                <>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "text-blue-400 font-bold" : "text-black-500 "
+                    }
+                    to="myorders"
+                  >
+                    <FaCarAlt className="inline-block mb-2 mr-2"></FaCarAlt>
+                    <p>My Orders</p>
+                  </NavLink>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "text-blue-400 font-bold" : "text-black-500 "
+                    }
+                    to="mywishlist"
+                  >
+                    <FaHeart className="inline-block mb-2 mr-2"></FaHeart>{" "}
+                    <p>My WishList</p>
+                  </NavLink>
+                </>
+              )}
+              {userRole?.role === "Admin" && (
+                <>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "text-blue-400 font-bold" : "text-black-500 "
+                    }
+                    to="mysellers"
+                  >
+                    <p className="mb-2">
+                      {" "}
+                      <FaUserAlt className="inline-block mb-2 mr-2"></FaUserAlt>{" "}
+                      My Sellers
+                    </p>
+                  </NavLink>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "text-blue-400 font-bold" : "text-black-500 "
+                    }
+                    to="mybuyers"
+                  >
+                    <p className="mb-2">
+                      {" "}
+                      <FaUserAlt className="inline-block mb-2 mr-2"></FaUserAlt>{" "}
+                      My Buyers
+                    </p>
+                  </NavLink>
+                </>
+              )}
+            </div>
             <button
               onClick={HandleLogOut}
               className="hover:bg-blue-400 bg-black duration-500 px-4 py-2 mt-3 rounded-md text-[16px] text-white"
